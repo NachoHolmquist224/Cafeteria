c72a1205565a3f6227b034dccc84a08e3989d0b0
Ignacio <ignacioholmquist12@gmail.com>
2026-06-07 20:35:38 -0300

    Agrego los endpoints restantes

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ app.get("/api/productos", (req, res) => {
   });
 });
 
+app.get("/api/promo", (req, res) => {
+  res.json({
+    mensaje:
+      "❗💙🤍HOY PROMO MUNDIALISTA💙🤍❗ ¡Cafe grande ☕ + tostado mixto a tan solo $6000❗❗",
+  });
+});
+
+app.get("/api/horarios", (req, res) => {
+  res.json({
+    mensaje:
+      "HORARIOS => Lunes a viernes: 10hs a 22hs - Sabado: 10hs a 20hs - Domingo: CERRADO",
+  });
+});
+
 console.log("hola mundo");
 console.log("El backend ya escucha");
 
